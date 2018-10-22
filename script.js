8e1adbd3efb623cc75edb090b3e3b261c0f6f1e7
Shayma <you@example.com>
2018-10-22 22:14:26 +0300

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var main = function() {
+  $(".btn").click(function() {
+    var post = $(".status-box").val();
+    $("<li>")
+      .text(post)
+      .prependTo(".posts");
+    $(".status-box").val("");
+
+    $(".btn").addClass("disabled");
+  });
+};
+$(".btn").addClass("disabled");
+$(document).ready(main);
